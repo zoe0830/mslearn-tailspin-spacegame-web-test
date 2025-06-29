5bc138a975f75dbcde8e0d48b824c6e4ff96c3d3
zoe zuo <zoestudio18@gmail.com>
2025-06-29 11:09:50 +1000

    updated

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -52,17 +52,17 @@ namespace TailSpin.SpaceGame.Web
         /// <param name="pageSize">The number of items on a page.</param>
         public Task<IEnumerable<T>> GetItemsAsync(
             Func<T, bool> queryPredicate,
-            Func<T, int> orderDescendingPredicate,
-            int page = 1, int pageSize = 10
-        )
+            Func<T, object> orderDescendingPredicate,
+            int page = 1,
+            int pageSize = 10)
         {
-            var result = _items
+            var items = _items
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
-                .Skip(page * pageSize) // find page
-                .Take(pageSize - 1); // take items
+                .Skip(page * pageSize) // skip to the requested page
+                .Take(pageSize); // take the requested number of items
 
-            return Task<IEnumerable<T>>.FromResult(result);
+            return Task<IEnumerable<T>>.FromResult(items);
         }
 
         /// <summary>
